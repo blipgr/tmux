275aff31a29b3c1f83a474f53ed7be76a252ec1b
blipgr <36671658+blipgr@users.noreply.github.com>
2024-10-14 03:15:19 +0300

    Update install_astronvim.sh

--- install_astronvim.sh
+++ install_astronvim.sh
@@ -3,7 +3,23 @@
 # Остановка выполнения скрипта при любой ошибке
 set -e
 
-# Шаг 1: Резервное копирование текущей конфигурации Neovim (если существует)
+# Шаг 1: Установка Bash-it
+echo "Установка Bash-it..."
+git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
+~/.bash_it/install.sh --silent
+source ~/.bashrc
+
+# Установка Neovim как редактора по умолчанию
+echo 'export EDITOR=vim' >> ~/.bashrc
+source ~/.bashrc
+
+# Включение alias и completion в bash-it
+echo "Включение alias и completion в bash-it..."
+bash-it enable alias apt
+bash-it enable completion git
+bash-it enable alias systemd
+
+# Шаг 2: Резервное копирование текущей конфигурации Neovim (если существует)
 echo "Резервное копирование текущей конфигурации Neovim..."
 if [ -d "$HOME/.config/nvim" ]; then
   mv ~/.config/nvim ~/.config/nvim.bak
@@ -21,7 +37,7 @@ if [ -d "$HOME/.cache/nvim" ]; then
   mv ~/.cache/nvim ~/.cache/nvim.bak
 fi
 
-# Шаг 2: Скачивание и установка Neovim версии 0.9.4
+# Шаг 3: Скачивание и установка Neovim версии 0.9.4
 echo "Скачивание и установка Neovim..."
 wget https://github.com/neovim/neovim/releases/download/v0.9.4/nvim.appimage
 chmod u+x nvim.appimage
@@ -39,7 +55,7 @@ sudo ln -s /usr/local/bin/nvim /usr/bin/nvim
 ls -l /usr/local/bin/nvim
 nvim --version
 
-# Шаг 3: Установка AstroNvim
+# Шаг 4: Установка AstroNvim
 echo "Клонирование репозитория AstroNvim..."
 git clone --depth 1 https://github.com/AstroNvim/template ~/.config/nvim
 
@@ -49,33 +65,17 @@ rm -rf ~/.config/nvim/.git
 # Запуск Neovim для первоначальной установки плагинов
 nvim +PackerSync
 
-# Шаг 4: Установка LSP и Treesitter
+# Шаг 5: Установка LSP и Treesitter
 echo "Установка LSP и Treesitter..."
 nvim --headless +LspInstall +TSInstall +qall
 
-# Шаг 5: Установка необходимых пакетов
+# Шаг 6: Установка необходимых пакетов
 echo "Установка ripgrep и npm..."
 sudo apt-get update
 sudo apt-get install -y ripgrep npm
 
 # Установка Tree-sitter CLI через npm
 npm install -g tree-sitter-cli
-
-# Шаг 6: Установка Bash-it
-echo "Установка Bash-it..."
-git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
-~/.bash_it/install.sh --silent
-source ~/.bashrc
-
-# Установка Neovim как редактора по умолчанию
-echo 'export EDITOR=nvim' >> ~/.bashrc
-source ~/.bashrc
-
-# Включение alias и completion в bash-it
-echo "Включение alias и completion в bash-it..."
-bash-it enable alias apt
-bash-it enable completion git
-bash-it enable alias systemd
 
 # Шаг 7: Установка дополнительных пакетов для ble.sh
 echo "Установка дополнительных пакетов для ble.sh..."
